152d6eb509c6a3877b37ecce89df832de34725a8
vrsvr <rifatvarisov@gmail.com>
2021-02-08 16:07:14 +0500

    deploy

---
+++ precache-manifest.ac203e45e7120106a9665763b24683f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f1ae711cd3ff16365f",
+    "url": "/rick-and-morty-characters/css/app.81d99fd2.css"
+  },
+  {
+    "revision": "47786aa5761a9c05a866",
+    "url": "/rick-and-morty-characters/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "684354d71683aaaa397f1b33330a5cf2",
+    "url": "/rick-and-morty-characters/index.html"
+  },
+  {
+    "revision": "28f1ae711cd3ff16365f",
+    "url": "/rick-and-morty-characters/js/app.522ef8a5.js"
+  },
+  {
+    "revision": "dcfbde480d899cc86db2",
+    "url": "/rick-and-morty-characters/js/chunk-2d22d746.e7e91a76.js"
+  },
+  {
+    "revision": "47786aa5761a9c05a866",
+    "url": "/rick-and-morty-characters/js/chunk-vendors.aaa5a0d2.js"
+  },
+  {
+    "revision": "c8867a5777dea168bb5f3e46fff60e87",
+    "url": "/rick-and-morty-characters/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rick-and-morty-characters/robots.txt"
+  }
+]);
